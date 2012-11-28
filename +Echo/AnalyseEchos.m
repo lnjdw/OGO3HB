573a25ca37c37e0a01916b38dc6c185e0d9f2a6b
Pascal Pieters <pascalaldo@gmail.com>
2012-11-28 16:04:59 +0100

    Improved volume determination
    Now also looks away from the long axis

--- +Echo/AnalyseEchos.m
+++ +Echo/AnalyseEchos.m
@@ -75,24 +75,41 @@ dddx2 = sqrt(a2^2+1);
     function [d d1 d2] = getDiameter(x,y)
         % Look to the left
         i = 1;
+        % Find the first point that is in the freehand drawing
+        while x-i > 0
+            if data_shape1(min(sz(1),max(1,round(-a2*i+y))), round(x-i)) == 1
+                break;
+            end
+            i = i+1;
+        end
+        istart = i;
         while x-i > 0
             if data_shape1(min(sz(1),max(1,round(-a2*i+y))), round(x-i)) == 0
                 break;
             end
             i = i+1;
         end
-        i = i-1;
+        i = i-istart;
         d1 = f1*i*dddx2; %sqrt((f1*-a2*i)^2+(f1*i)^2);
 
         % Look to the right
         j = 1;
+        % Find the first point that is in the freehand drawing
+        while x+j < sz(2)
+            if data_shape1(min(sz(1),max(1,round(a2*j+y))), round(x+j)) == 1
+                break;
+            end
+            j = j+1;
+        end
+        jstart = j;
+        % Find the first point that is zero
         while x+j < sz(2)
             if data_shape1(min(sz(1),max(1,round(a2*j+y))), round(x+j)) == 0
                 break;
             end
             j = j+1;
         end
-        j = j-1;
+        j = j-jstart;
         d2 = f1*j*dddx2; %sqrt((f1*a2*j)^2+(f1*j)^2);
         
         % Calculate the total diameter
